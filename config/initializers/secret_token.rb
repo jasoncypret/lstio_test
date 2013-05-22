5a25821e205f2f30fe388dac4452bf5d91ac3940
Jason Cypret <jason@jasoncypret.com>
2013-05-22 12:06:58 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lstio::Application.config.secret_token = '2eb60f41626e0f8368c3088c4867f3cb7618cf4364cba33175cf0934d13006de307ca4191e7e5b91d7d0682d7d59384e88bd38bf80dd859f3496889da65c8fa0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lstio::Application.config.session_store :cookie_store, key: '_lstio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lstio::Application.config.session_store :active_record_store
